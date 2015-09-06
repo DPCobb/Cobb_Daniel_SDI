e6b0958bbc706695c2da690cc8417d7bc3421503
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-05 21:10:21 -0400

    Name and Trip Length
    Ask user for name and trip length, print to console

--- Cobb_Daniel_Expressions_Assignment/js/script.js
+++ Cobb_Daniel_Expressions_Assignment/js/script.js
@@ -1,1 +1,16 @@
-alert("test")
+/*
+Daniel Cobb
+SDI Section 01
+Expressions Assignment
+9-5-15
+ */
+
+//alert("test")
+
+// Calculate fuel cost and how many tanks of gas a trip will take
+// Ask user for their name and tell them what we will be calculating
+var user = prompt("We are going to calculate the cost of your trip.\nPlease enter your name");
+console.log(user);
+// Ask user for length of trip in miles
+var tripLen = prompt(user + " enter the length of your trip");
+console.log(tripLen + " miles total");
