0a04bbabd2a490dd03ac9325a4f45dc615f1ce58
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-05 21:21:25 -0400

    Added Variables
    Added remaining variables needed and printed to console.

--- Cobb_Daniel_Expressions_Assignment/js/script.js
+++ Cobb_Daniel_Expressions_Assignment/js/script.js
@@ -11,6 +11,19 @@ Expressions Assignment
 // Ask user for their name and tell them what we will be calculating
 var user = prompt("We are going to calculate the cost of your trip.\nPlease enter your name");
 console.log(user);
+
 // Ask user for length of trip in miles
 var tripLen = prompt(user + " enter the length of your trip in miles");
 console.log(tripLen + " miles total");
+
+// Ask for vehicles estimated miles per gallon
+var estMpg = prompt("Enter your vehicles estimated miles per gallon");
+console.log(estMpg + " mpg");
+
+// Ask for vehicles fuel tank size
+var tankSize = prompt("Enter your gas tank capacity");
+console.log(tankSize + " total gallons per full tank");
+
+// Ask for average cost of fuel per gallon
+var fuelCost = prompt("Enter average cost of fuel per gallon");
+console.log(fuelCost + " dollars per gallon");
