63b53a820fd5aea4471720f71bbafb384571d529
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-09 18:03:28 -0400

    Expressions Worksheet
    Added all activities

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -40,3 +40,34 @@ alert("Oops we can't forget about Sparky. Each guest can only eat a full slice, 
  console.log("Sparky will get " + sliceSparky + " slices.");
  alert("Sparky will get " + sliceSparky + " slices.");
 
+// Average Shopping Bill
+ // Set grocery bill amounts
+ var bill = [100, 130, 95, 125, 75];
+ // Add bill totals
+ var billTot = bill[0] + bill[1] + bill[2] + bill[3] + bill[4];
+ console.log(billTot + " total on groceries over 5 weeks");
+ // Divide bill totals by 5 to get average
+ billAvg = billTot / 5;
+ console.log(billAvg + " average bill");
+ // Display results
+ alert("You spent $" + billTot + " on groceries over the last 5 weeks, this averages out to $" + billAvg + " per trip");
+
+//Discounts
+ //Ask for original price
+ var price = prompt("Please enter the original price:");
+ console.log(price + " original cost");
+ //set discount of 25% 100 - 25 = 75% of original cost
+ var discPer = 25;
+ var discPrice = .75;
+ // Set sales tax
+ var saleTax = .07;
+ //Calculate price w/o sales tax
+ var totalNoTax = price * discPrice;
+ console.log("Price with discount applied and no sales tax: $" + totalNoTax);
+ //Calculate price w/tax
+ var tax = totalNoTax * saleTax;
+  var totalTax = tax + totalNoTax;
+ console.log("Total cost with sales tax: $" + totalTax);
+ // Display original cost, discount %, discounted price without sales tax, and discounted price with sales tax.
+alert("Your item was originally $" + price + " with a " + discPer + "% discount it is now $" + totalNoTax + " without sales tax and $" + totalTax + " with sales tax.");
+
