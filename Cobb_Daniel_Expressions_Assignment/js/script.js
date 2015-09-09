c1cbcf1d60fbe3d02ed82a865bd93af6bf6db116
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-09 15:47:58 -0400

    Clean up code
    Cleaned up code and added arrays

--- Cobb_Daniel_Expressions_Assignment/js/script.js
+++ Cobb_Daniel_Expressions_Assignment/js/script.js
@@ -8,6 +8,7 @@ Expressions Assignment
 //alert("test")
 
 // Calculate fuel cost and how many tanks of gas a trip will take
+
 // Ask user for their name and tell them what we will be calculating
 var user = prompt("We are going to calculate the cost of your trip.\nPlease enter your name");
 console.log(user);
@@ -37,49 +38,51 @@ var totTank = (tripLen/estMpg) / tankSize;
 console.log(totTank + " tanks of gas");
 
 // Limit results of costTrip to hundredths
-var costTrip = costTrip.toFixed(2);
+costTrip = costTrip.toFixed(2);
 console.log (costTrip + " dollars to complete trip rounded");
 
 // Limit tanks to tenths of a gallon
-var totTank = totTank.toFixed(1);
+totTank = totTank.toFixed(1);
 console.log(totTank + " tanks of gas rounded");
 
 // Tell user the cost of their trip and how many tanks of fuel
+var tripOut = ["Your trip will cost $" + costTrip + " and use about " + totTank + " tank of gas.", "Your trip will cost $" + costTrip + " and use less than 1 tank of gas.", "Your trip will cost $" + costTrip + " and use about " + totTank + " tanks of gas."];
 if (totTank == 1){
-    alert("Your trip will cost " + costTrip + " dollars and use about " + totTank + " tank of gas.");}
+    alert(tripOut[0]);}
 else if (totTank <=1){
-    alert("Your trip will cost " + costTrip + " dollars and use less than 1 tank of gas.");}
+    alert(tripOut[1]);}
 else{
-    alert("Your trip will cost " + costTrip + " dollars and use about " + totTank + " tanks of gas.");};
-console.log(user + " your trip will cost " + costTrip + " dollars and use about " + totTank + " tanks of gas.");
+    alert(tripOut[2]);}
+console.log(user + " your trip will cost $" + costTrip + " and use about " + totTank + " tanks of gas.");
 
 // Add 5 miles for rest stops, refueling, breaks etc
-var tripLen = parseInt(tripLen);
-var tripLen = tripLen += 5;
+tripLen = parseInt(tripLen);
+tripLen += 5;
 
 //Recalculate values
-console.log("Add 5 miles to account for rest breaks, refuels, detours etc")
+console.log("Add 5 miles to account for rest breaks, refuels, detours etc");
 // Calculate the cost of the trip by total gallons of gas: (tripLen/estMpg)*fuelCost
-var costTrip = (tripLen/estMpg) * fuelCost;
-console.log(costTrip + " dollars to complete trip");
+var costTripMod = (tripLen/estMpg) * fuelCost;
+console.log(costTripMod + " dollars to complete trip");
 
 // Calculate how many tanks of gas your trip will take
-var totTank = (tripLen/estMpg) / tankSize;
-console.log(totTank + " tanks of gas");
+var totTankMod = (tripLen/estMpg) / tankSize;
+console.log(totTankMod + " tanks of gas");
 
 // Limit results of costTrip to hundredths
-var costTrip = costTrip.toFixed(2);
-console.log (costTrip + " dollars to complete trip rounded");
+costTripMod = costTripMod.toFixed(2);
+console.log (costTripMod + " dollars to complete trip rounded");
 
 // Limit tanks to tenths of a gallon
-var totTank = totTank.toFixed(1);
-console.log(totTank + " tanks of gas rounded");
+totTankMod = totTankMod.toFixed(1);
+console.log(totTankMod + " tanks of gas rounded");
 
 // Tell user the cost of their trip and how many tanks of fuel with 5 extra miles for stops and detours
-if (totTank == 1){
-    alert("Accounting for stops and detours, your trip will cost " + costTrip + " dollars and use about " + totTank + " tank of gas.");}
-else if (totTank <=1){
-    alert("Accounting for stops and detours, your trip will cost " + costTrip + " dollars and use less than 1 tank of gas.");}
+var tripOutMod = ["Accounting for stops and detours, your trip will cost $" + costTripMod + " and use about " + totTankMod + " tank of gas.", "Accounting for stops and detours, your trip will cost $" + costTripMod + " and use less than 1 tank of gas.", "Accounting for stops and detours, your trip will cost $" + costTripMod + " and use about " + totTankMod + " tanks of gas."];
+if (totTankMod == 1){
+    alert(tripOutMod[0]);}
+else if (totTankMod <=1){
+    alert(tripOutMod[1]);}
 else{
-    alert("Accounting for stops and detours, your trip will cost " + costTrip + " dollars and use about " + totTank + " tanks of gas.");};
-console.log(user + " your trip will cost " + costTrip + " dollars and use about " + totTank + " tanks of gas.");
+    alert(tripOutMod[2]);}
+console.log(user + " your trip will cost $" + costTripMod + " and use about " + totTankMod + " tanks of gas.");
