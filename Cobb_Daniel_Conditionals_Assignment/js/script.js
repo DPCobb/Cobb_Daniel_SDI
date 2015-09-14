5e0c16c2d79ee922c1e19ea6a222c2cf303a4994
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-13 20:18:54 -0400

    addedPrompts
    Added prompts for weights and began calculations

--- Cobb_Daniel_Conditionals_Assignment/js/script.js
+++ Cobb_Daniel_Conditionals_Assignment/js/script.js
@@ -10,3 +10,16 @@ September 2013, 2015
 //Explain to the user what we are doing and get their name.
 var user = prompt("We are going to calculate the weight of your truck\nFirst off, what's your name?");
 console.log(user + " is the current user.");
+//Ask the user for the weight of their unloaded truck
+var truckEmpty = prompt(user + " what is the weight of your truck while empty?");
+console.log(user + "'s truck weighs " + truckEmpty + " lbs while empty");
+//Ask the user for the weight of their load
+var loadWeight = prompt(user + " what is the weight of the load you will be carrying in tons?");
+console.log(user + " will be carrying " + loadWeight + "tons");
+//Set ton to lbs
+var lbs = parseInt(loadWeight)*2000;
+console.log(loadWeight + " tons is " + lbs + " lbs.")
+//Get total truck weight by combining empty weight and load weight
+var truckTotal = parseInt(truckEmpty) + parseInt(lbs);
+console.log(truckTotal + " lbs is the total weight of the truck");
+ 
