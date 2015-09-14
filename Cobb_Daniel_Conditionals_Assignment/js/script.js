c98e829f7353fb6e7658c9440fbb4a11347f40b7
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-13 20:07:16 -0400

    intitalSetup
    Initial setup and test with alert. Added first prompt.

---
+++ Cobb_Daniel_Conditionals_Assignment/js/script.js
@@ -0,0 +1,12 @@
+
+ /*
+Daniel Cobb
+SDI Section 01
+Conditionals Assignment
+September 2013, 2015
+ */
+
+//alert("test")
+//Explain to the user what we are doing and get their name.
+var user = prompt("We are going to calculate the weight of your truck\nFirst off, what's your name?");
+console.log(user + " is the current user.");
