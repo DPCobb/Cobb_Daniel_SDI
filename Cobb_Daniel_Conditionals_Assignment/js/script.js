b0dcd4f75b16d29b7699e712a7057a0f0ed72453
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-14 19:14:22 -0400

    checkTon
    Added check to make sure loadWeight is entered in tons not lbs

--- Cobb_Daniel_Conditionals_Assignment/js/script.js
+++ Cobb_Daniel_Conditionals_Assignment/js/script.js
@@ -15,6 +15,9 @@ var truckEmpty = prompt(user + " what is the weight of your truck while empty?")
 console.log(user + "'s truck weighs " + truckEmpty + " lbs while empty");
 //Ask the user for the weight of their load
 var loadWeight = prompt(user + " what is the weight of the load you will be carrying in tons?");
+//check loadWeight is tons and not lbs
+var loadCheck = loadWeight.length;
+(loadCheck > 2) ?  loadWeight = prompt(user + " please re enter the weight of your load in tons") : console.log("Weight ok");
 console.log(user + " will be carrying " + loadWeight + "tons");
 //Set ton to lbs
 var lbs = parseInt(loadWeight)*2000;
@@ -22,4 +25,3 @@ console.log(loadWeight + " tons is " + lbs + " lbs.")
 //Get total truck weight by combining empty weight and load weight
 var truckTotal = parseInt(truckEmpty) + parseInt(lbs);
 console.log(truckTotal + " lbs is the total weight of the truck");
- 
