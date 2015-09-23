596b231114ed02bce6f8903540101ca7d64d2cdf
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-23 15:15:00 -0400

    console.log/functions
    Elaborated console.log outputs and changed functions to round miles/hr
    and removed final out put alert

--- Cobb_Daniel_Functions_Assignment/js/script.js
+++ Cobb_Daniel_Functions_Assignment/js/script.js
@@ -51,15 +51,19 @@ September 22, 2015
 // Ask user for the information need for calculations and validate using inputValidation
  var startMiles = prompt(user + " please enter your starting mileage.");
  startMiles = inputValidation(startMiles);
+ console.log( user + "'s starting mileage was " + startMiles + ".");
 
  var endMiles = prompt(user + " please enter your ending mileage.");
  endMiles = inputValidation(endMiles);
+ console.log( user + "'s ending mileage was " + endMiles + ".");
 
  var startTime = prompt(user + " please enter what time you starting driving, rounded to the nearest hour in 24hr format");
  startTime = inputValidation(startTime);
+ console.log( user + " started driving at " + startTime + ".");
 
  var endTime = prompt(user + " please enter what time you stopped driving, rounded to the nearest hour in 24hr format");
  endTime = inputValidation(endTime);
+ console.log( user + " stopped driving at " + endTime + ".");
 
  //Set Function for total mile computation
 
@@ -71,7 +75,7 @@ September 22, 2015
  //Call Function to calculate total miles and display results in console.log
 
  var trip = totalMiles(startMiles, endMiles);
- console.log(trip);
+ console.log(user + "'s trip was a total of " + trip + " miles.");
 
  //Set Function to calculate total hours driven
 
@@ -83,27 +87,30 @@ September 22, 2015
  //Call Function to calculate total hours driven
 
  var hours = totalHours(startTime, endTime);
- console.log(hours);
+ console.log( user + "'s total drive time was " + hours + " hours.");
 
  //Set Anon Function to calculate average miles driven each hour
 
  var milesPerHour = function(miles, hour){
-     var avgMiles = miles / hour;
+     var avgMiles = Math.round(Number(miles) / Number(hour));
      return avgMiles;
  };
 
  //Call milesPerHour function
  var tripOutPut = milesPerHour(trip,hours);
- console.log(tripOutPut + " mph");
+ console.log( user + " drove on average " + tripOutPut + " miles each hour.");
 
  //Set anon function to give final information to user
 
  var userInfo = function(totMiles, totHours, avgMPH, name){
-     alert(name + " you drove a total of " + totMiles + " total miles over " + totHours + " total hours at an average of " + avgMPH + " miles driven per hour.");
+     var out = name + " you drove a total of " + totMiles + " total miles over " + totHours + " total hours at an average of " + avgMPH + " miles driven per hour.";
+     return out;
  };
 
  //Call userInfo for final display of information
 
- userInfo(trip, hours,tripOutPut,user);
+ var finalOutput = userInfo(trip, hours,tripOutPut,user);
+ alert(finalOutput);
+ console.log(finalOutput);
 
 
