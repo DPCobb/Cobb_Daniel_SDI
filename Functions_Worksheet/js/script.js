4638ee0aaea9433c37c485477edd9eeabbffaefd
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-24 13:28:27 -0400

    circumferenceProblem
    Finished circumference function

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -8,4 +8,18 @@
 
 //alert("test");
 
+//Circumference of a Circle
+//C = 2(pi)(r)
+
+//Set radius variable
+var radius = 2;
+//create function to return circumference
+function circOfCircle(r){
+    return circumference = 2 * 3.14 * r;
+}
+//Call circOfCircle
+var ans = circOfCircle(radius);
+//Log ans
+console.log("The circumference is " + ans);
+
 
