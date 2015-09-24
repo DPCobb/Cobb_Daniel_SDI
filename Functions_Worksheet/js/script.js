44edc37881d2d499eef18a6f5a2d17030dfaf772
Dan Cobb <danielcobb@daniels-MacBook-Pro.local>
2015-09-24 13:39:38 -0400

    stung
    Finished stung problem

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -12,7 +12,7 @@
 //C = 2(pi)(r)
 
 //Set radius variable
-var radius = 2;
+var radius = prompt("Enter radius:");
 //create function to return circumference
 function circOfCircle(r){
     return circumference = 2 * 3.14 * r;
@@ -20,6 +20,17 @@ function circOfCircle(r){
 //Call circOfCircle
 var ans = circOfCircle(radius);
 //Log ans
-console.log("The circumference is " + ans);
+console.log("The circumference of the circle is " + ans);
 
+//Stung
 
+//Set variables for weight and stings
+var weight = prompt("Enter victim weight:");
+var stings = 8.666666667;
+//Set Function to calculate how many bee stings it would take to kill an animal
+function totalStings(vicWeight, beeStings){
+    return Math.round(beeStings * vicWeight);
+}
+//Call totalStings
+var fatalStings = totalStings(weight, stings);
+console.log("It takes " + fatalStings +" beestings to kill this animal.");
